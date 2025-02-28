bbf780e4d7f6fc80213c0471c14cbbb2ee6589de
Khurram Ali <khurramcali@gmail.com>
2025-02-28 14:52:01 -0500

    env fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,8 @@
 import type { Book } from "@repo/database";
 
-const API_URL = "http://localhost:3000";
+const API_URL = process.env.API_URL
+  ? `${process.env.API_URL}`
+  : "http://localhost:3000";
 
 export default async function Page() {
   const res = await fetch(`${API_URL}/`);
